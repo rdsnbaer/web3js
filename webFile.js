b92f186f537c8b1bcf01615df29f78f396f0a209
woodbaer <woodbaer@baerchain.net>
2019-07-09 11:27:53 +0800

    add web3 functional

---
+++ webFile.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var app = express();
+
+app.use('/', express.static("public"));
+// app.use('/packages', express.static("public"));
+
+app.listen(8100, function(){
+	console.log("xxxxx")
+})
